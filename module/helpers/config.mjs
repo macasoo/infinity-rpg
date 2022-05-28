8bcaa91b7fbfe7d3f40942397e30292a2998d3ec
macasoo <macaso47@gmail.com>
2022-05-29 01:04:12 +0200

    first commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,25 @@
+export const INFINITY = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ INFINITY.abilities = {
+  "agi": "INFINITY.AbilityAgi",     //vorher Str - Strenght
+  "awa": "INFINITY.AbilityAwa",     // vorher Dex- Dexterity
+  "brw": "INFINITY.AbilityBrw",     // vorher Con- Constitution
+  "coo": "INFINITY.AbilityCoo",     // vorher Int- Intelligenz
+  "int": "INFINITY.AbilityInt",     // vorher Wis- Wisdom
+  "per": "INFINITY.AbilityPer",     // vorher Cha- Charisma
+  "wil": "INFINITY.AbilityWil"      // vorher -
+};
+
+INFINITY.abilityAbbreviations = {
+  "agi": "INFINITY.AbilityAgiAbbr",     //vorher Str - Strenght
+  "awa": "INFINITY.AbilityAwaAbbr",     // vorher Dex- Dexterity
+  "brw": "INFINITY.AbilityBrwAbbr",     // vorher Con- Constitution
+  "coo": "INFINITY.AbilityCooAbbr",     // vorher Int- Intelligenz
+  "int": "INFINITY.AbilityIntAbbr",     // vorher Wis- Wisdom
+  "per": "INFINITY.AbilityPerAbbr",     // vorher Cha- Charisma
+  "wil": "INFINITY.AbilityWilAbbr"      // vorher -
+};
